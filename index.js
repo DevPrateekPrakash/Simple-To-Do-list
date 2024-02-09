cda5519553ab803f262015452418654a99ada8bb
Prateek Prakash <65021420+DevPrateekPrakash@users.noreply.github.com>
2024-02-10 00:41:34 +0530

    js connected
    js connected, add task function added

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 alert("Js is connected")
+
+const addForm= document.querySelector(".add")
+const task= document.querySelector(".tasks")
+
+
+
+
+addForm.addEventListener("submit", event=>{
+    event.preventDefault()
+    const value=addForm.task.value.trim()
+    if(value.length){
+        console.log(value);
+        task.innerHTML+=`<li>
+        <span class="task">${value}</span>
+        <i class="bi bi-trash delete"></i> 
+    </li>`
+        addForm.reset();
+    }
+    
+})
