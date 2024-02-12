502792b0bd56d0aaf753e888f0fd2f545f402212
Prateek Prakash <65021420+DevPrateekPrakash@users.noreply.github.com>
2024-02-12 22:14:27 +0530

    index js tasks event listeners added
    delete clear function is implemented

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 alert("Js is connected")
-
 const addForm= document.querySelector(".add")
-const task= document.querySelector(".tasks")
+const tasks= document.querySelector(".tasks")
+const clearAll= document.querySelector(".clear")
+const messageSpan=document.querySelector(".message span")
+const searchForm=document.querySelector(".search")
 
+function updatetask(){
+    const taskno = tasks.children.length
+    messageSpan.textContent=`You have ${taskno} pending task`
+}
 
-
+updatetask()
 
 addForm.addEventListener("submit", event=>{
     event.preventDefault()
     const value=addForm.task.value.trim()
     if(value.length){
-        console.log(value);
-        task.innerHTML+=`<li>
+        tasks.innerHTML+=`<li>
         <span class="task">${value}</span>
         <i class="bi bi-trash delete"></i> 
     </li>`
         addForm.reset();
+        updatetask()
     }
     
+})
+
+tasks.addEventListener("click", event=>{
+    event.preventDefault()
+    if(event.target.classList.contains("delete")){
+        console.log("delete button clicked")
+        event.target.parentElement.remove();
+        updatetask()
+    }
+})
+
+clearAll.addEventListener("click", event=>{
+    const taskItems=tasks.querySelectorAll("li")
+    taskItems.forEach(item =>{
+        item.remove()
+        updatetask()
+    })
+})
+
+searchForm.addEventListener("keyup", event=>{
+    const term=searchForm.task.value.trim()
 })
